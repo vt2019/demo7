d91a608f48437926fc5b1ca11e811e71fbd1c035
Alexander Birk <birk@pingworks.de>
2018-12-14 18:50:07 +0100

    first commit

---
+++ src/test/java/com/example/demo7/HomePageControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.demo7;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class HomePageControllerTest {
+
+	@Test
+	public void testHome() {
+        
+	}
+
+}
